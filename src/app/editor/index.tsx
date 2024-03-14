868281d5c4c959c6c23b226225f42547aa014989
Vimulatus <vasu.mit2501@gmail.com>
2024-03-14 16:59:33 +0530

    added Reader and Editor skeleton

---
+++ src/app/editor/index.tsx
@@ -0,0 +1,11 @@
+import Toolbar from "./toolbar"
+import Canvas from './canvas'
+
+export default function Editor() {
+    return (
+        <div>
+            <Toolbar />
+            <Canvas />
+        </div>
+    )
+}
---
+++ src/app/reader/index.tsx
@@ -0,0 +1,5 @@
+export default function Reader() {
+    return (
+        <div></div>
+    )
+}
